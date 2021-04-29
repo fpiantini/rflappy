b1e23ec7489fc70555665b700406b3641f788ac0
Francesco Piantini <francesco.piantini@thalesgroup.com>
2021-04-29 13:23:19 +0200

    added GameMode to state

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,48 @@
 use bracket_lib::prelude::*;
 
-struct State {}
+enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
 
+struct State {
+    mode: GameMode,
+}
+
+impl State {
+    fn new() -> Self {
+        Self {
+            mode: GameMode::Menu,
+        }
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        // TODO: fill this stub later
+        ctx.cls();
+        ctx.print(1, 1, "STATE: Menu");
+        self.mode = GameMode::Playing;
+    }
+    fn playing(&mut self, ctx: &mut BTerm) {
+        // TODO: fill this stub later
+        ctx.print(1, 2, "STATE: Playing");
+        self.mode = GameMode::End;
+
+    }
+    fn end_game(&mut self, ctx: &mut BTerm) {
+        // TODO: fill this stub later
+        ctx.print(1, 3, "STATE: End");
+
+    }
+}
+
+// GameState trait cames from Bracket
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
-        ctx.cls();
-        ctx.print(1, 1, "Hello Bracket terminal!");
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.playing(ctx),
+            GameMode::End => self.end_game(ctx),
+        }
     }
 }
 
@@ -13,5 +50,5 @@ fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Flappy Dragon")
         .build()?;
-    main_loop(context, State{})
+    main_loop(context, State::new())
 }
