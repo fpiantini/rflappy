b158e4449eaebf33ec89d1499929d75e74d6b1db
Francesco Piantini <francesco.piantini@gmail.com>
2021-04-30 08:32:56 +0200

    added player, first skeleton of game with gravity and flapping

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,76 @@
 use bracket_lib::prelude::*;
 
+const SCREEN_WIDTH: i32 = 80;
+const SCREEN_HEIGH: i32 = 50;
+const FRAME_DURATION: f32 = 75.0;
+
 enum GameMode {
     Menu,
     Playing,
     End,
 }
 
+// --------------------------------------------------------------------
 struct State {
     mode: GameMode,
+    frame_time: f32,
+    player: Player,
 }
-
 impl State {
     fn new() -> Self {
-        Self {
+        State {
             mode: GameMode::Menu,
+            frame_time: 0.0,
+            player: Player::new(5, 25),
         }
     }
     fn main_menu(&mut self, ctx: &mut BTerm) {
         // TODO: fill this stub later
         ctx.cls();
-        ctx.print(1, 1, "STATE: Menu");
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game)");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        if self.player.y > SCREEN_HEIGH {
+            self.mode = GameMode::End;
+        }
+    }
+    fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
         self.mode = GameMode::Playing;
     }
-    fn playing(&mut self, ctx: &mut BTerm) {
-        // TODO: fill this stub later
-        ctx.print(1, 2, "STATE: Playing");
-        self.mode = GameMode::End;
-
-    }
-    fn end_game(&mut self, ctx: &mut BTerm) {
-        // TODO: fill this stub later
-        ctx.print(1, 3, "STATE: End");
-
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "GAME OVER");
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game)");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
 }
 
@@ -40,11 +79,51 @@ impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         match self.mode {
             GameMode::Menu => self.main_menu(ctx),
-            GameMode::Playing => self.playing(ctx),
-            GameMode::End => self.end_game(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
         }
     }
 }
+// --------------------------------------------------------------------
+
+// --------------------------------------------------------------------
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: f32,
+}
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Player {
+            x,
+            y,
+            velocity: 0.0,
+        }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(
+            0,
+            self.y,
+            YELLOW,
+            BLACK,
+            to_cp437('@')
+        );
+    }
+    fn gravity_and_move(&mut self) {
+        if self.velocity < 2.0 {
+            self.velocity += 0.2;
+        }
+        self.y += self.velocity as i32;
+        self.x += 1;
+        if self.y < 0{
+            self.y = 0;
+        }
+    }
+    fn flap(&mut self) {
+        self.velocity = -2.0;
+    }
+}
+// --------------------------------------------------------------------
 
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
