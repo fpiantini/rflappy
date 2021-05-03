504aeb42d0d8df8d77b30578b93301a268e52d83
Francesco Piantini <francesco.piantini@gmail.com>
2021-05-03 23:42:40 +0200

    try to modularize

---
+++ src/flappy/mod.rs
@@ -0,0 +1,102 @@
+use crate::prelude::*;
+mod player;
+use player::*;
+
+mod obstacle;
+use obstacle::*;
+
+// --------------------------------------------------------------------
+pub enum GameMode {
+    Menu,
+    Playing,
+    End,
+}
+
+// --------------------------------------------------------------------
+pub struct State {
+    mode: GameMode,
+    frame_time: f32,
+    player: Player,
+    obstacle: Obstacle,
+    score: i32,
+}
+impl State {
+    pub fn new() -> Self {
+        State {
+            mode: GameMode::Menu,
+            frame_time: 0.0,
+            player: Player::new(5, 25),
+            obstacle: Obstacle::new(SCREEN_WIDTH, 0),
+            score: 0,
+        }
+    }
+    fn main_menu(&mut self, ctx: &mut BTerm) {
+        // TODO: fill this stub later
+        ctx.cls();
+        ctx.print_centered(5, "Welcome to Flappy Dragon");
+        ctx.print_centered(8, "(P) Play Game");
+        ctx.print_centered(9, "(Q) Quit Game)");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+    fn play(&mut self, ctx: &mut BTerm) {
+        ctx.cls_bg(NAVY);
+        self.frame_time += ctx.frame_time_ms;
+        if self.frame_time > FRAME_DURATION {
+            self.frame_time = 0.0;
+            self.player.gravity_and_move();
+        }
+        if let Some(VirtualKeyCode::Space) = ctx.key {
+            self.player.flap();
+        }
+        self.player.render(ctx);
+        ctx.print(0, 0, "Press SPACE to flap.");
+        ctx.print(0, 1, &format!("Score: {}", self.score));
+        self.obstacle.render(ctx, self.player.x);
+        if self.player.x > self.obstacle.x {
+            self.score += 1;
+            self.obstacle = Obstacle::new(self.player.x + SCREEN_WIDTH, self.score);
+        }
+        if self.player.y > SCREEN_HEIGH || self.obstacle.hit_obstable(self.player.x, self.player.y)
+        {
+            self.mode = GameMode::End;
+        }
+    }
+    fn restart(&mut self) {
+        self.player = Player::new(5, 25);
+        self.frame_time = 0.0;
+        self.obstacle = Obstacle::new(SCREEN_WIDTH, 0);
+        self.mode = GameMode::Playing;
+        self.score = 0;
+    }
+    fn dead(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print_centered(5, "GAME OVER");
+        ctx.print_centered(6, &format!("You earned {} points", self.score));
+        ctx.print_centered(8, "(P) Play Again");
+        ctx.print_centered(9, "(Q) Quit Game)");
+        if let Some(key) = ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
+    }
+}
+
+// GameState trait cames from Bracket
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        match self.mode {
+            GameMode::Menu => self.main_menu(ctx),
+            GameMode::Playing => self.play(ctx),
+            GameMode::End => self.dead(ctx),
+        }
+    }
+}
